6741e5f7d218609d84e3839db7dee5836be9347b
Daniel Noble <daniel.wa.noble@gmail.com>
2016-10-30 16:53:54 +1100

    Small updates to documentatons and scripts

--- R/data.R
+++ R/data.R
@@ -1,22 +1,119 @@
 
-#' birdegg_ssd.8
+#' @title birdegg_ssd.8 data
 #'
-#' Data set on bird egg sexual size dimorphism meta-analysis. Data was taken from Rutkowska et al. (2014). In this study more than one egg came from the same study which introduces non-independence in the data (i.e. correlated structure). As a sensitivity analysis Rutkowska et al. (2014) used an effective sample size proposed by Higgins and Green (2009), which requires an estimate of the intra-class correlation coefficient (ICC) in the calculation of effect sizes. In this case, an ICC estimate of 0.8 was used. 
-#' @format A raw csv file 
-#' @references
-#' Rutkowska, J., Dubiec, A. and Nakgawa, S. (2014). All eggs are made equal: a meta-analysis of egg sexual size dimorphism. Journal of Evolutionary Biology, 27:153-160
-#' Higgins, J.P.T. and Green, S. (2009). Cochran handbook for systematic reviews and interventions. Wiley-Blackwell.
+#' @description Data set on bird egg sexual size dimorphism meta-analysis. Data was taken from Rutkowska et al. (2014). In this study more than one egg came from the same study which introduces non-independence in the data (i.e. correlated structure). As a sensitivity analysis Rutkowska et al. (2014) used an effective sample size proposed by Higgins and Green (2009), which requires an estimate of the intra-class correlation coefficient (ICC) in the calculation of effect sizes. In this case, an ICC estimate of 0.8 was used. 
+#' @format A raw csv file containing the following columns and descriptors
+#' \enumerate{
+#' \item animal:	species Latin name
+#' \item Spp:	species common name
+#' \item Lndim:	logaritm of adult mass dimorphism
+#' \item Measure:	method of eggs measurments
+#' \item Neggs:	number of eggs
+#' \item Nclutches:	number of clutches
+#' \item ESr:	effect size, correlation
+#' \item Type:	how the effect size was calculated: row data/inferential statistics
+#' \item StudyID:	reference
+#' \item Year:	year of publication
+#' \item D:	design effect
+#' \item EN:	effective sample size
+#' \item Zr:	Fisher's Z
+#' \item VZr:	sampling variance
+#'}	
+#' @references Rutkowska, J., Dubiec, A. and Nakgawa, S. (2014). All eggs are made equal: a meta-analysis of egg sexual size dimorphism. Journal of Evolutionary Biology, 27:153-160
+#' @references Higgins, J.P.T. and Green, S. (2009). Cochran handbook for systematic reviews and interventions. Wiley-Blackwell. pgs 412-414.
 #' @name birdegg_ssd.8
 #' @docType data
 NULL
 
-#' birdegg_ssd.5
+#' @title birdegg_ssd.5 data
 #'
-#' Data set on bird egg sexual size dimorphism meta-analysis. Data was taken from Rutkowska et al. (2014). In this study more than one egg came from the same study which introduces non-independence in the data (i.e. correlated structure). As a sensitivity analysis Rutkowska et al. (2014) used an effective sample size proposed by Higgins and Green (2009), which requires an estimate of the intra-class correlation coefficient (ICC) in the calculation of effect sizes. In this case, an ICC estimate of 0.5 was used.
-#' @format A raw csv file 
-#' @references
-#' Rutkowska, J., Dubiec, A. and Nakgawa, S. (2014). All eggs are made equal: a meta-analysis of egg sexual size dimorphism. Journal of Evolutionary Biology, 27:153-160
-#' Higgins, J.P.T. and Green, S. (2009). Cochran handbook for systematic reviews and interventions. Wiley-Blackwell.
-#' @name birdegg_ssd.8
+#' @description Data set on bird egg sexual size dimorphism meta-analysis. Data was taken from Rutkowska et al. (2014). In this study more than one egg came from the same study which introduces non-independence in the data (i.e. correlated structure). As a sensitivity analysis Rutkowska et al. (2014) used an effective sample size proposed by Higgins and Green (2009), which requires an estimate of the intra-class correlation coefficient (ICC) in the calculation of effect sizes. In this case, an ICC estimate of 0.5 was used.
+#' @format A raw csv file containing the following columns and descriptors
+#' \enumerate{
+#' \item animal:	species Latin name
+#' \item Spp:	species common name
+#' \item Lndim:	logaritm of adult mass dimorphism
+#' \item Measure:	method of eggs measurments
+#' \item Neggs:	number of eggs
+#' \item Nclutches:	number of clutches
+#' \item ESr:	effect size, correlation
+#' \item Type:	how the effect size was calculated: row data/inferential statistics
+#' \item StudyID:	reference
+#' \item Year:	year of publication
+#' \item D:	design effect
+#' \item EN:	effective sample size
+#' \item Zr:	Fisher's Z
+#' \item VZr:	sampling variance
+#'}	
+#' @references Rutkowska, J., Dubiec, A. and Nakgawa, S. (2014). All eggs are made equal: a meta-analysis of egg sexual size dimorphism. Journal of Evolutionary Biology, 27:153-160
+#' @references Higgins, J.P.T. and Green, S. (2009). Cochran handbook for systematic reviews and interventions. Wiley-Blackwell. pgs 412-414.
+#' @name birdegg_ssd.5
+#' @docType data
+NULL
+
+#' @title matdiet
+#'
+#' @description Data set on how maternal diet impacts copying styles in rodents. Data was taken from Besson et al. (2016). 
+#' @format A raw csv file containing the following columns and descriptors
+#' \enumerate{
+#' \item 
+#' \item 
+#' \item 
+
+#'}	
+#' @references Besson, A. A., Lagisz, M., Senior, A.M., Hector, K.L. and Nakagawa, S. (2016). Effect of maternal diet on offspring coping styles in rodents: a systematic review and meta-analysis. Biological Reviews, 91:1065-1080
+#' @name matdiet
+#' @docType data
+NULL
+
+#' @title moSizetradeoff
+#'
+#' @description Data set investing the correlations between maternal size and offspring size (dataset 1 - M-O.size). Data was taken from Lim et al. (2014). 
+#' @format A raw csv file containing the following columns and descriptors
+#' \enumerate{
+#' \item 
+#' \item 
+#'}	
+#' @references Lim, J. N., Senior, A. M. & Nakagawa, S. (2014) Heterogeneity in individual quality and reproductive trade-offs within species. Evolution. 68: 2306–2318
+#' @name tradeoffs
+#' @docType data
+NULL
+
+
+#' @title moNumtradeoff
+#' @description Data set investing the correlations between maternal size and offspring number (dataset 2 - M-O.Number). Data was taken from Lim et al. (2014). 
+#' @format A raw csv file containing the following columns and descriptors
+#' \enumerate{
+#' \item 
+#' \item 
+#'}	
+#' @references Lim, J. N., Senior, A. M. & Nakagawa, S. (2014) Heterogeneity in individual quality and reproductive trade-offs within species. Evolution. 68: 2306–2318
+#' @name moNumtradeoff
+#' @docType data
+NULL
+
+
+#' @title ooAdjtradeoff
+#' @description Data set investing the correlations between offspring size and offspring number after adjusting for maternal size(dataset 3 - O-O.Adjusted). Data was taken from Lim et al. (2014). 
+#' @format A raw csv file containing the following columns and descriptors
+#' \enumerate{
+#' \item 
+#' \item 
+#'}	
+#' @references Lim, J. N., Senior, A. M. & Nakagawa, S. (2014) Heterogeneity in individual quality and reproductive trade-offs within species. Evolution. 68: 2306–2318
+#' @name ooAdjtradeoff
+#' @docType data
+NULL
+
+
+#' @title ooUnAdjtradeoff
+#' @description Data set investing the correlations between offspring size and offspring number not adjusting for maternal size (dataset 3 - O-O.Unadjusted). Data was taken from Lim et al. (2014). 
+#' @format A raw csv file containing the following columns and descriptors
+#' \enumerate{
+#' \item 
+#' \item 
+#'}	
+#' @references Lim, J. N., Senior, A. M. & Nakagawa, S. (2014) Heterogeneity in individual quality and reproductive trade-offs within species. Evolution. 68: 2306–2318
+#' @name ooUnAdjtradeoff
 #' @docType data
 NULL
--- R/foldnorm.R
+++ R/foldnorm.R
@@ -1,18 +1,35 @@
 
-#' Applying the folded normal distribution using the posterior parameter estimates of effect sizes that are normally distributed. This function will allow one to understand the overall magnitude of effect regardless of effect size direction. 
-#' @param mu The posterior distribution of mean estimates from an MCMCglmm object.
-#' @param sd The standard deviation of the posterior distribution of variance-covariance matrix from an MCMCglmm object.
-#' @param type Indicates whether the posterior mean (i.e. "mean") or mode (i.e. "mode") should be returned.
-#' @return Posterior mean or mode along with the 95% credible intervals (i.e. the highest posterior density interval - HPDinterval) 
+#' @title Folded normal distribution
+#'
+#' @description Applying the folded normal distribution using the posterior parameter estimates of effect sizes that are normally distributed. This function will allow one to understand the overall magnitude of effect regardless of effect size direction. 
+#' @param mu The posterior distribution of mean estimates from an MCMCglmm object. Alternatively, you can give it the mean and sd of a normal distribution and it will provide the mean (only used if type = "raw").
+#' @param sd The standard deviation of the posterior distribution of variance-covariance matrix from an MCMCglmm object or just the sd of a normal distribution. 
+#' @param type Indicates whether the posterior mean (i.e. "mean") or mode (i.e. "mode") should be returned. Alternatively, if one knows the mean and the sd of you can use type = "raw" to get the mean and variance of the folded normal distribution.
+#' @return Posterior mean or mode along with the 95 percent credible intervals (i.e. the highest posterior density interval - HPDinterval). Alternatively if type = "raw" it will return the mean and variance for the folded normal distribution.
+#' @references Morrisey, M.B. 2016. Meta-analysis of magnitudes, differences and variation in evolutionary parameters. Journal of Evolutionary Biology, 29:1882-1904
+#' @references Morrisey, M.B. 2016. Rejoinder: Further considerations for meta-analysis of transformed quantities such as absolute values. Journal of Evolutionary Biology, 29:1922-1931
 #' @author Daniel Noble - daniel.noble@unsw.edu.au
 #' @examples
-#' dir <- paste0(system.file("extdata",  package="ibutt"), "/")
-#' data <- ibuttProc(dir, date = "2015-11-06", time = c("10:00", "11:00"), list = TRUE)
+#' set.seed(60)
+#' x <- rnorm(1000, 1, 1)
+#' mean(x)
+#' sd(x)
+#' # Calculate the mean of the folded norm from raw x
+#' foldnorm(mu = mean(x), sd = sd(x), type = "raw")
+#' #Should be close to: 
+#' mean(abs(x))
+#' var(abs(x))
 #' @export
 
-foldnorm <-  function(mu, sd, type = c("mean", "mode")){
+foldnorm <-  function(mu, sd, type = c("mean", "mode", "raw")){
 	
-	postfnorm <- dnorm(mu, 0, sd)*2*sd^2 + mu*(2*pnorm(mu, 0, sd) -1)
+     postfnorm <- dnorm(mu, 0, sd)*2*sd^2 + mu*(2*pnorm(mu, 0, sd) -1)
+
+	if(type == "raw"){
+		    est <- postfnorm
+		    var.fnorm <- mu^2 + sd^2 - (sd*sqrt(2/pi)*exp((-1*mu^2)/(2*sd^2)) + mu*(1-2*pnorm(-1*mu/sd, 0, 1)))^2
+		    est <- data.frame(Mean=est, Variance = var.fnorm)
+	}
 
 	if(type == "mean"){
 		mean <- mean(postfnorm)
