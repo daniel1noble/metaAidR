b53ca05a915046ea00149f5edda0c4c8470f5b01
Daniel Noble <daniel.wa.noble@gmail.com>
2016-12-08 15:45:41 +1100

    More tests for I2 added. Mainly check that recognising correct objects is good.

--- tests/testthat/test-I2.R
+++ tests/testthat/test-I2.R
@@ -3,6 +3,7 @@ context("Tests I2 function")
 test_that("Check that output is correct", {
 	library(metafor)
 	library(MCMCglmm)
+	library(lme4)
 	
 	#Simulate some meta-analytic data
 	set.seed(123)
@@ -10,18 +11,23 @@ test_that("Check that output is correct", {
 	spN = 100
 	stdyN =50 
 	rgN = 10
+	SDsp = 0.5
+	SDsty = 4
+	SDrg = 3
+	SDe = 1
 
 	V <- rgamma(n, shape = 1, rate = 10)
 	muEs <- rnorm(n, 3, sqrt(V))
-	spp <- rep(rnorm(spN, 0, 0.5), each = n / spN)
-	stdy <- rep(rnorm(stdyN, 0, 4), each = n / stdyN)
-	rg <- rep(rnorm(rgN, 0, 3), each = n / rgN)
+	spp <- rep(rnorm(spN, 0, SDsp), each = n / spN)
+	stdy <- rep(rnorm(stdyN, 0, SDsty), each = n / stdyN)
+	rg <- rep(rnorm(rgN, 0, SDrg), each = n / rgN)
+	e <- rnorm(n, 0, SDe)
 
-	e <- rnorm(n, 0, 1)
-
+	#Simulated data 1
 	es <- muEs + spp + stdy + e
 	data <- data.frame(es, spp = as.factor(spp), stdy = as.factor(stdy), V, obs = 1:length(es))
 
+	#Simulated data 2
 	es2 <- muEs + spp + stdy + rg + e
 	data2 <- data.frame(es, spp = as.factor(spp), stdy = as.factor(stdy), rg = as.factor(rg), V, obs = 1:length(es))
 
@@ -29,11 +35,12 @@ test_that("Check that output is correct", {
 	wi <- 1/V  #weight
 	Vw <- sum((wi) * (length(wi) - 1))  / (((sum(wi)^2) - sum((wi)^2)))
 	
-	I2spp <- (0.5^2) / (4^2 + 0.5^2 + Vw + 1)
-	I2st <- (4^2) / (4^2 + 0.5^2 + Vw + 1)
-	I2T <- (4^2 + 0.5^2 + 1) / (4^2 + 0.5^2 + Vw + 1)
+	I2spp <- (SDsp^2) / (SDsty^2 + SDsp^2 + Vw + SDe)
+	
+	I2st <- (SDsty^2) / (SDsty^2 + SDsp^2 + Vw + SDe)
+	I2st2 <- (SDsty^2) / (SDsty^2 + SDrg^2 + SDsp^2 + Vw + SDe)
 
-	I2st2 <- (4^2) / (4^2 + 3^2 + 0.5^2 + Vw + 1)
+	I2T <- (SDsty^2 + SDsp^2 + SDe) / (SDsty^2 + SDsp^2 + Vw + SDe)
 
 	#This model estimates a residual variance
 	metaFor <- metafor::rma.mv(es, V, random = list(~1|spp, ~1|stdy, ~1|obs), struct="UN",data = data)
@@ -45,17 +52,23 @@ test_that("Check that output is correct", {
 	metaMCMC <- MCMCglmm::MCMCglmm(es~1, random = ~spp + stdy, mev = V, data = data)
 	metaMCMC2 <- MCMCglmm::MCMCglmm(es~1, random = ~spp + stdy + rg, mev = V, data = data2)
 
+	# Wrong model!
+	lmER <- lme4::lmer(es ~1 + (1|spp) + (1|stdy), weights = V, data = data)
+
+	# Testing the I2 function
 	MetaF <- I2(metaFor, v = data$V)
 	MetaF2 <- I2(metaFor2, v = data$V)
 
 	MCMC <- I2(metaMCMC, v = data$V, phylo = FALSE)
 	MCMC2 <- I2(metaMCMC2, v = data$V, phylo = FALSE)
 
+	#Tests
 	expect_equal(as.numeric(MetaF[rownames(MetaF) == "spp",][1]) ,  I2spp, tolerance = 0.01, info = "faildMetaspp")
 	expect_equal(as.numeric(MetaF[rownames(MetaF) == "stdy",][1]) ,  I2st, tolerance = 0.05, info = "faildMetastudy")
 	expect_equal(as.numeric(MetaF[rownames(MetaF) == "total",][1]) ,  I2T, tolerance = 0.05, info = "faildMetatot")
 	expect_equal(dim(MetaF2),  c(4,3), info = "dimMetaforFail")
-	expect_error(I2(metaFor3, v = data$V))
+	expect_error(I2(metaFor3, v = data$V), info = "failedMetafor_no_obs")
+	expect_error(I2(lmER, v = data$V), info = "failedWrongModel")
 
 	expect_equal(as.numeric(MCMC[rownames(MCMC) == "spp",][1]) ,  I2spp, tolerance = 0.005, info = "faildMCspp")
 	expect_equal(as.numeric(MCMC[rownames(MCMC) == "stdy",][1]) ,  I2st, tolerance = 0.05, info = "faildMCstudy")
